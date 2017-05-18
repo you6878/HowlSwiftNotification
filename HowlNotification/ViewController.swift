8d0834c0c2ee04eee79366f031e44885a4339881
MyeongSic <you6878@icloud.com>
2017-05-18 22:30:45 +0900

    init

--- HowlNotification/ViewController.swift
+++ HowlNotification/ViewController.swift
@@ -7,11 +7,14 @@
 //
 
 import UIKit
+import UserNotifications
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UNUserNotificationCenterDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        UNUserNotificationCenter.current().delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +23,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func notificationButton(_ sender: Any) {
+    
+    
+        let content = UNMutableNotificationContent()
+        content.title = NSString.localizedUserNotificationString(forKey:
+            "Hello!", arguments: nil)
+        content.body = NSString.localizedUserNotificationString(forKey:
+            "Hello_message_body", arguments: nil)
+        
+        // Deliver the notification in five seconds.
+        content.sound = UNNotificationSound.default()
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1,
+                                                        repeats: false)
+        
+        // Schedule the notification.
+        let request = UNNotificationRequest(identifier: "FiveSecond", content: content, trigger: trigger)
+        let center = UNUserNotificationCenter.current()
+        center.add(request, withCompletionHandler: nil)
+        
+        
+        
+    }
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert, .sound])
+    }
+    
 
 }
 
